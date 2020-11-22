63fdfde6ab10313fde94fab25d8f4c5035334055
julianbooher <julianbooher@hotmail.com>
2020-11-22 14:48:28 -0600

    changed some sql initializiation

--- database.sql
+++ database.sql
@@ -1,10 +1,11 @@
 -- if you need to drop the table
 DROP TABLE todos;
+
 -- creates the todos table, inside of the weekend-to-do-app db.
 CREATE TABLE todos(
 	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR (12) NOT NULL,
-	"todo" VARCHAR (120) NOT NULL,
+	"name" VARCHAR (20) NOT NULL,
+	"todo" VARCHAR (80) NOT NULL,
 	"date_added" VARCHAR (20),
 	"date_completed" VARCHAR (20)
 );
@@ -15,5 +16,5 @@ INSERT INTO todos (name, todo, date_added) VALUES ('Teaghan', 'Clean the bathroo
 INSERT INTO todos (name, todo, date_added) VALUES ('Henri', 'Clean the cat litter.', '11/20/20');
 INSERT INTO todos (name, todo, date_added) VALUES ('Miro', 'Play with the string.', '11/20/20');
 
--- Current select as of 11/21/20
-SELECT * FROM todos ORDER BY date_completed DESC, date_added, id;
+-- Current select as of 11/22/20
+SELECT * FROM todos ORDER BY date_added DESC, id;
